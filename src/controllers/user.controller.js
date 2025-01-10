eac5d2fdb9717fb141b05fa6768614999da7b760
strfunctionk <comzun01@gmail.com>
2025-01-10 16:30:56 +0900

    회원가입 구현

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,9 +3,85 @@ import { bodyToUser } from "../dtos/user.dto.js";
 import { userSignUp } from "../services/user.service.js";
 
 export const handleUserSignUp = async (req, res, next) => {
+    /*
+      #swagger.summary = '회원 가입 API';
+      #swagger.requestBody = {
+        required: true,
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                name: { type: "string" },
+                nickname: { type: "string" },
+                email: { type: "string" },
+                password: { type: "string"},
+                profileImage: { type: "string" },
+                status: { type: "string" },
+                socialType: { type: "string" },
+                inactiveDate: { type: "string",  format: "date"  },
+                artists: {type: "array", items: { type: "number" } },
+                genres: { type: "array", items: { type: "number" } }
+              }
+            }
+          }
+        }
+      };
+      #swagger.responses[200] = {
+        description: "회원 가입 성공 응답",
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                resultType: { type: "string", example: "SUCCESS" },
+                error: { type: "object", nullable: true, example: null },
+                success: {
+                  type: "object",
+                  properties: {
+                    name: { type: "string" },
+                    nickname: { type: "string" },
+                    email: { type: "string" },
+                    password: { type: "string"},
+                    profileImage: { type: "string" },
+                    status: { type: "string" },
+                    socialType: { type: "string" },
+                    inactiveDate: { type: "string",  format: "date"  },
+                    artists: {type: "array", items: { type: "number" } },
+                    genres: { type: "array", items: { type: "number" } }
+                  }
+                }
+              }
+            }
+          }
+        }
+      };
+      #swagger.responses[400] = {
+        description: "회원 가입 이메일 중복 실패 응답",
+        content: {
+          "application/json": {
+            schema: {
+              type: "object",
+              properties: {
+                resultType: { type: "string", example: "FAIL" },
+                error: {
+                  type: "object",
+                  properties: {
+                    errorCode: { type: "string", example: "U001" },
+                    reason: { type: "string" },
+                    data: { type: "object" }
+                  }
+                },
+                success: { type: "object", nullable: true, example: null }
+              }
+            }
+          }
+        }
+      };
+    */
     try {
         console.log("회원가입을 요청했습니다!");
-        console.log("body:", req.body); // 값이 잘 들어오나 확인하기 위한 테스트용
+        console.log("body:", req.body);
 
         const user = await userSignUp(bodyToUser(req.body));
         res.status(StatusCodes.OK).success(user);
--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -7,7 +7,64 @@ export const addUser = async (data) => {
     const created = await prisma.user.create({ data: data });
     return created.id;
 };
+
 export const getUser = async (userId) => {
     const user = await prisma.user.findFirstOrThrow({ where: { id: userId } });
     return user;
+};
+
+export const setUserArtist = async (userId, artistId) => {
+    const artist = await prisma.artist.findFirst({ where: { id: artistId } });
+    if (!artist) {
+        return null;
+    }
+    await prisma.userArtist.create({
+        data: {
+            userId: userId,
+            artistId: artistId
+        }
+    })
+}
+
+export const getUserArtistId = async (userId) => {
+    const preferences = await prisma.userArtist.findMany({
+        select: {
+            id: true,
+            userId: true,
+            user: true,
+            artistId: true,
+            artist: true,
+        },
+        where: { userId: userId },
+        orderBy: { artistId: "asc" }
+    });
+    return preferences;
+};
+
+export const setUserGenre = async (userId, genreId) => {
+    const genre = await prisma.genre.findFirst({ where: { id: genreId } });
+    if (!genre) {
+        return null;
+    }
+    await prisma.userGenre.create({
+        data: {
+            userId: userId,
+            genreId: genreId
+        }
+    })
+}
+
+export const getUserGenreId = async (userId) => {
+    const preferences = await prisma.userGenre.findMany({
+        select: {
+            id: true,
+            userId: true,
+            user: true,
+            genreId: true,
+            genre: true,
+        },
+        where: { userId: userId },
+        orderBy: { genreId: "asc" }
+    });
+    return preferences;
 };
