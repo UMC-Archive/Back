f6c88e96765f0f3b023016ec06d82ebc1804e9e6
SangHun Kwon <jun00739@naver.com>
2025-01-17 22:15:25 +0900

    fix email auth
    이메일 인증번호 전송 gmail로 교체

--- src/middleware/email.js
+++ src/middleware/email.js
@@ -8,23 +8,24 @@ const mailSender = {
 	// 메일발송 함수 (보낸사람의 대표 이름, 받는주소, 제목, html문서)
 	sendGmail: (toEmail, code) => {
 		var transporter = nodemailer.createTransport({
-			service: "naver", // 메일 보내는 곳
+			service: "gmail", // 메일 보내는 곳
 			prot: 587, //gmail 의 경우 587을 사용함
-			host: "smtp.naver.com",
+			host: "smtp.gmail.com",
 			secure: false,
 			requireTLS: true,
 			auth: {
-				user: process.env.NAVER_EMAIL, // 보내는 메일의 주소
-				pass: process.env.NAVER_SECRET, // 보내는 메일의 비밀번호
-			},
-			tls : {
-				rejectUnauthorized : false
+				user: process.env.AUTH_EMAIL, // 보내는 메일의 주소
+				pass: process.env.AUTH_PASSWORD, // 보내는 메일의 비밀번호
 			}
+			// ,
+			// tls : {
+			// 	rejectUnauthorized : false
+			// }
 		});
 
 		// 메일 옵션
 		var mailOptions = {
-			from: "Archive" + " <" + "jun00739@naver.com" + ">", // 보낸사람의 메일주소 및 이름
+			from: "Archive" + " <" + process.env.AUTH_EMAIL + ">", // 보낸사람의 메일주소 및 이름
 			to: toEmail, // 수신할 이메일
 			subject: "[Archive] 회원가입 인증 메일입니다.", // 메일 제목
 			//text: text // 메일 내용
