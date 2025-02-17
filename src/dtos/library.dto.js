1e3d1431db0a4f90cb998d6dd479a8d364df3fc7
strfunctionk <comzun01@gmail.com>
2025-02-17 16:28:59 +0900

    feat:add album

--- src/dtos/library.dto.js
+++ src/dtos/library.dto.js
@@ -1,20 +1,50 @@
 export const responseFromAllMusics = (musics) => {
   return musics.map((data) => ({
-    id: data.music.id,
-    title: data.music.title,
-    releaseTime: new Date(data.music.releaseTime).getFullYear(),
-    image: data.music.image,
+    music: {
+      id: data.music.id,
+      title: data.music.title,
+      releaseTime: new Date(data.music.releaseTime).getFullYear(),
+      image: data.music.image,
+    },
+    album: {
+      id: data.music.album.id,
+      title: data.music.album.title,
+      releaseTime: new Date(data.music.album.releaseTime).getFullYear(),
+      image: data.music.album.image,
+    },
     artist: data.music.MusicArtists[0].artist.name,
   }));
 };
 
 export const responseFromAllArtists = (artists) => {
-  return artists.map((data) => ({
-    id: data.artist.id,
-    name: data.artist.name,
-    image: data.artist.image,
-  }));
+  return artists.map((data) => {
+    const uniqueAlbums = data.artist.MusicArtists.reduce((acc, albumData) => {
+      const album = albumData.music.album;
+
+      // album.id가 acc에 존재하지 않으면 추가하고, acc의 길이가 10 미만인 경우에만 추가
+      if (!acc.some((item) => item.id === album.id) && acc.length < 10) {
+        acc.push({
+          id: album.id,
+          title: album.title,
+          image: album.image,
+          releaseTime: new Date(album.releaseTime).getFullYear(),
+        });
+      }
+
+      return acc;
+    }, []); // 초기값은 빈 배열
+
+    return {
+      artist: {
+        id: data.artist.id,
+        name: data.artist.name,
+        image: data.artist.image,
+      },
+      album: uniqueAlbums,
+    };
+  });
 };
+
 
 export const responseFromAllAlbums = (albums) => {
   console.log("dto albums : " + JSON.stringify(albums, null, 2));
